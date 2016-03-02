0dc33437843b324844af8fe3edded56dae1cf4bd
mrmcc3 <mrmcc3@gmail.com>
2016-03-02 10:26:09 +1000

    blank project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mrmcc3/frontend-build-components "0.1.0-SNAPSHOT"
+  :description "Frontend Build Components"
+  :url "https://github.com/mrmcc3/frontend-build-components"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ src/fbc/core.clj
@@ -0,0 +1,4 @@
+(ns fbc.core)
+
+(println "hi")
+
