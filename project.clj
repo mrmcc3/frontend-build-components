5611653caa9e2beddda92946a6165579e000b4c5
mrmcc3 <mrmcc3@gmail.com>
2016-03-03 09:38:08 +1000

    next release

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject mrmcc3/frontend-build-components "0.1.0"
+(defproject mrmcc3/frontend-build-components "0.1.1-SNAPSHOT"
   :description "Components for building front-end projects with clojure"
   :url "https://github.com/mrmcc3/frontend-build-components"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [com.stuartsierra/component "0.3.1"]
