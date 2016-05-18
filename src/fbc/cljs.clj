bb491c2b45f20deea1f61dfa2a7fcd7443592164
Michael McClintock <mrmcc3@gmail.com>
2016-05-18 22:55:46 +1000

    remove watch from cljs

--- src/fbc/cljs.clj
+++ src/fbc/cljs.clj
@@ -1,8 +1,7 @@
 (ns fbc.cljs
   (:require [com.stuartsierra.component :as component]
             [clojure.java.io :as io]
-            [cljs.build.api :as cljs]
-            [cljs.env :as env]))
+            [cljs.build.api :as cljs]))
 
 (defn clean [{:keys [output-to output-dir]}]
   (when (and output-to output-dir)
@@ -10,31 +9,16 @@
                        (reverse (file-seq (io/file output-dir))))]
       (when (.exists file) (.delete file)))))
 
-(defrecord CljsCompiler [inputs opts watch stop-fn started]
+(defrecord CljsCompiler [inputs opts]
   component/Lifecycle
   (start [this]
-    (if started
-      this
-      (do
-        (cljs/build inputs opts)
-        (assoc this
-          :started true
-          :stop-fn (when watch
-                     (let [stop (atom nil)
-                           cenv (if-not (nil? env/*compiler*)
-                                  env/*compiler*
-                                  (env/default-compiler-env opts))
-                           futr (future (cljs/watch inputs opts cenv stop))]
-                       (fn []
-                         (reset! stop true)
-                         (future-cancel futr))))))))
+    (cljs/build inputs opts)
+    this)
   (stop [this]
-    (when stop-fn (stop-fn))
-    (when started (clean opts))
-    (assoc this :started nil :stop-fn nil)))
+    (clean opts)
+    this))
 
 (defn cljs-compiler [cfg]
   (map->CljsCompiler
     {:inputs (apply cljs/inputs (get-in cfg [:cljs :source-paths]))
-     :opts   (get-in cfg [:cljs :compiler])
-     :watch  (get-in cfg [:cljs :watch])}))
+     :opts   (get-in cfg [:cljs :compiler])}))
