568ccc5e11d52b56486eef75582214798ca20c31
Michael McClintock <mrmcc3@gmail.com>
2016-06-04 20:27:46 +1000

    bump deps

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.8.51" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.36" :scope "provided"]
+                 [com.cognitect/transit-clj "0.8.285"]
                  [com.stuartsierra/component "0.3.1"]
                  [suspendable "0.1.1"]
-                 [figwheel-sidecar "0.5.3-1"]
+                 [figwheel-sidecar "0.5.3-2"]
                  [mrmcc3/libsass-clj "0.1.5"]])
