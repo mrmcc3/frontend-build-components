345f85cddb8b9ee9bd0aceed20b5e67a51a965e1
Michael McClintock <mrmcc3@gmail.com>
2016-06-26 12:51:33 +1000

    use strings for build id's

--- src/fbc/figwheel.clj
+++ src/fbc/figwheel.clj
@@ -9,35 +9,36 @@
     (when-not (fig/figwheel-running?)
       (fig/start-figwheel! config))
     (when-not autobuild
-      (fig/build-once :default))
+      (fig/build-once "default"))
     this)
   (stop [this]
     (when (fig/figwheel-running?)
       (when autobuild
-        (fig/stop-autobuild :default))
-      (fig/clean-builds :default)
+        (fig/stop-autobuild "default"))
+      (fig/clean-builds "default")
       (fig/stop-figwheel!))
     this)
   Suspendable
   (suspend [this]
     (when (fig/figwheel-running?)
       (when autobuild
-        (fig/stop-autobuild :default)))
+        (fig/stop-autobuild "default")))
     this)
   (resume [this old]
     (if (fig/figwheel-running?)
       (if (= (:config this) (:config old))
         (if autobuild
-          (fig/start-autobuild :default)
-          (fig/build-once :default))
+          (fig/start-autobuild "default")
+          (fig/build-once "default"))
         (do (component/stop old) (component/start this)))
       (component/start this))
     this))
 
+
 (defn figwheel [cfg]
   (let [autobuild (get-in cfg [:figwheel :autobuild])
-        build-id (if autobuild :default :no-autobuild)
-        build (assoc (:cljs cfg) :id :default :figwheel autobuild)
+        build-id (if autobuild "default" "no-autobuild")
+        build (assoc (:cljs cfg) :id "default" :figwheel autobuild)
         options (dissoc (:figwheel cfg) :autobuild)]
     (map->Figwheel
       {:autobuild autobuild
