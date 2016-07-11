384192944f5de2fceeac11bc43454b7e4c8ef7da
Michael McClintock <mrmcc3@gmail.com>
2016-07-11 14:28:03 +1000

    bump deps. release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mrmcc3/frontend-build-components "0.1.3-SNAPSHOT"
+(defproject mrmcc3/frontend-build-components "0.1.3"
   :description "Components for building front-end projects with clojure"
   :url "https://github.com/mrmcc3/frontend-build-components"
   :license {:name "Eclipse Public License"
@@ -9,6 +9,6 @@
                  [com.cognitect/transit-clj "0.8.285"]
                  [com.stuartsierra/component "0.3.1"]
                  [suspendable "0.1.1"]
-                 [figwheel-sidecar "0.5.4-4"]
-                 [mrmcc3/libsass-clj "0.1.7"]
+                 [figwheel-sidecar "0.5.4-7"]
+                 [mrmcc3/libsass-clj "0.1.8"]
                  [hawk "0.2.10"]])
